009c4e42a68259851a6bb370ec1ef911ed67be91
elizabeth41elias <82070269+elizabeth41elias@users.noreply.github.com>
2021-04-06 21:41:18 -0500

    Update CtrlAlumno.js

--- js/CtrlAlumno.js
+++ js/CtrlAlumno.js
@@ -52,6 +52,9 @@ async function busca() {
       const data = doc.data();
       forma.matricula.value = data.matricula;
       forma.nombre.value = data.nombre || "";
+      forma.telefono.value = data.telefono || "";
+      forma.grupo.value = data.grupo || "";
+      forma.fecha.value = data.fecha || "";
       forma.addEventListener(
         "submit", guarda);
       forma.eliminar.
@@ -75,15 +78,20 @@ async function guarda(evt) {
       new FormData(forma);
     const matricula = getString(
         formData, "matricula").trim();  
-    const nombre = getString(
-      formData, "nombre").trim();
+    const nombre = getString(formData, "nombre").trim();
+    const telefono = getString(formData, "telefono").trim();
+    const grupo = getString(formData, "grupo").trim();
+    const fecha = getString(formData, "fecha").trim();
     /**
      * @type {
         import("./tipos.js").
                 Alumno} */
     const modelo = {
       matricula, 
-      nombre
+      nombre,
+      telefono,
+      grupo,
+      fecha
     };
     await daoAlumno.
       doc(id).
