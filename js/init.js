a46e6f7ac8f6670460d32f499977ecc22fb007e1
elizabeth41elias <82070269+elizabeth41elias@users.noreply.github.com>
2021-04-06 21:24:07 -0500

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,9 @@
+firebase.initializeApp({
+    apiKey: "AIzaSyAHGVksYZUqiFCrWE5U8igyWQuZH6fa934",
+    authDomain: "alumnos41eeg.firebaseapp.com",
+    projectId: "alumnos41eeg",
+    storageBucket: "alumnos41eeg.appspot.com",
+    messagingSenderId: "334683487355",
+    appId: "1:334683487355:web:7a347ea9a6ee3eefef76de",
+    measurementId: "G-VQFG29CYKL"
+});
